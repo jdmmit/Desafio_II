b601e099f750311e35c327f5948b9f3bd80346a0
jdmmit <juandavidmurillomejia0718@outlook.es>
2025-10-21 21:18:02 -0500

    Se implementaron empeso a implementar Sistemaudeatunes

--- Sistemaudeatunes.h
+++ Sistemaudeatunes.h
@@ -1,10 +1,42 @@
 #ifndef SISTEMAUDEATUNES_H
 #define SISTEMAUDEATUNES_H
 
+#include <iostrem>
+#include <string>
+#include "Lista.h"
+#include "Usuario.h"
+#include "Artista.h"
+#include "MensajePublicitario.h"
+
+using namespace std;
+
+
 class SistemaUdeATunes
 {
+private:
+    Lista<Usuario> usuarios;
+    Lista<Artista> artista;
+    Lista<MensajePublicitario> mensaje;
+    Usuario* usuarioActual;
+    int contadorIteraciones;
+
 public:
     SistemaUdeATunes();
+
+    // Metodos
+    void cargarDatos();
+    void iniciarSesion();
+    void reproduccionAleatoria();
+    void menuListasFavoritos();
+    long cularMemoria();
+    void registrarUsuario();
+    void mostrarFavoritos();
+    void agregarCancionFavoritas();
+    void removerCancionFavorita();
+    void menuPrincipal();
+    void mostrarUsuariosRegistrados();
+
+
 };
 
-#endif // SISTEMAUDEATUNES_H
+#endif
